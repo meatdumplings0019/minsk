e6eb92c447c5f33fc8702aa48f99cf76d0ba2690
ndrzy <dandan0019@outlook.com>
2025-08-19 09:45:35 +0800

    number

--- mc/CodeAnalysis/LiteralExpressionSyntax.cs
+++ mc/CodeAnalysis/LiteralExpressionSyntax.cs
@@ -4,7 +4,7 @@ namespace Minsk.CodeAnalysis;
 public sealed class LiteralExpressionSyntax(SyntaxToken literalToken) : ExpressionSyntax
 {
     public SyntaxToken LiteralToken { get; } = literalToken;
-    public override SyntaxKind Kind => SyntaxKind.NumberExpression;
+    public override SyntaxKind Kind => SyntaxKind.LiteralExpression;
     
     public override IEnumerable<SyntaxNode> GetChildren()
     {
