00be2785e1dd3a960a25cd01d1a8912e951cf0e1
ndrzy <dandan0019@outlook.com>
2025-12-15 23:05:33 +0800

    feat(mc): Lexer
    - 添加Lexer

--- mc/Program.cs
+++ mc/Program.cs
@@ -1,7 +1,21 @@
-﻿while (true)
+﻿namespace mc;
+
+class Program
 {
-    Console.Write("> ");
-    var line = Console.ReadLine();
-    if (string.IsNullOrWhiteSpace(line))
-        return;
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("> ");
+            var line = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(line))
+                return;
+        }
+    }
+}
+
+class Lexer(string text)
+{
+    private readonly string _text = text;
+    private int _position;
 }
